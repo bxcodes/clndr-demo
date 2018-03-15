2fb1ad4dd43405d3aeba3268c60e172e0897767e
bxcodes <boxuana@163.com>
2018-03-15 15:53:32 -0400

    03/15

--- demo/reg.php
+++ demo/reg.php
@@ -4,6 +4,19 @@ include_once("sql.php");
 
 global $db;
 
-$db->addUser($_POST['email'], $_POST['firstname'], $_POST['lastname']);
+header("Access-Control-Allow-Origin: *");
+
+$action = isset($_POST['action']) ? $_POST['action'] : '';
+
+switch($action){
+	case 'register': 
+	$db->addUser($_POST['email'], $_POST['firstname'], $_POST['lastname']);
+	break;
+	default:
+	echo 'API Action Error';
+	
+}
+
+
 
 ?>

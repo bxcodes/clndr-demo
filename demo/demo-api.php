61278aeece492fc74646b1b1914980e76c8d5ca7
bxcodes <boxuana@163.com>
2018-03-16 16:40:19 -0400

    03/16

--- demo/demo-api.php
+++ demo/demo-api.php
@@ -12,6 +12,9 @@ switch($action){
 	case 'register': 
 	$db->addUser($_POST['email'], $_POST['firstname'], $_POST['lastname']);
 	break;
+	case 'update': 
+	$db->changePassword($_POST['email'], $_POST['password1'], $_POST['password2']);
+	break;
 	default:
 	echo 'API Action Error';
 	
